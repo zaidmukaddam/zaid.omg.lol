7415ebb0f58801a9a9941f3ee9718277ea4c978b
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2022-11-25 19:01:20 +0530

    feat: add arrow back

--- pages/418.tsx
+++ pages/418.tsx
@@ -11,7 +11,7 @@ export default function Custom418(){
             <h1 className="mt-10 text-4xl tracking-[-0.02em] [font-variation-settings:'wght'_700] -sm:text-3xl -sm:leading-[1.1111]">418 - I&apos;m a teapot</h1>
             <p className="mt-3 text-sm opacity-70 mb-3">The server refuses to brew coffee because it is, permanently, a teapot.</p>
             {/* back to home button */}
-            <Link href="/" className="px-4 py-2 text-white bg-[#1da1f2] rounded-md hover:bg-[#1a91da]">Back to Home</Link>
+            <Link href="/" className="px-4 py-2 text-white bg-[#1da1f2] rounded-md hover:bg-[#1a91da]">Back to Home ←</Link>
         </Container>
     );
 }
--- pages/500.tsx
+++ pages/500.tsx
@@ -11,7 +11,7 @@ export default function Custom500() {
             <h1 className="mt-10 text-4xl tracking-[-0.02em] [font-variation-settings:'wght'_700] -sm:text-3xl -sm:leading-[1.1111]">500 - Internal Server Error</h1>
             <p className="mt-3 text-sm opacity-70 mb-3">The server encountered an unexpected condition that prevented it from fulfilling the request.</p>
             {/* back to home button */}
-            <Link href="/" className="px-4 py-2 text-white bg-[#1da1f2] rounded-md hover:bg-[#1a91da]">Back to Home</Link>
+            <Link href="/" className="px-4 py-2 text-white bg-[#1da1f2] rounded-md hover:bg-[#1a91da]">Back to Home ←</Link>
         </Container>
     );
 }

2058e8451abd93b6b68f50df033f198edd609618
Zaid Mukaddam <zaidaltafmukaddam@gmail.com>
2022-11-27 17:50:45 +0530

    feat: update block request message

--- pages/api/blocked.ts
+++ pages/api/blocked.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  return res.status(429).json({ message: 'Too many requests' });
+  return res.status(429).json({ message: `Don't DDoS me pls 🥺` });
 }
